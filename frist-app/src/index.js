9f65cbfae7839908c4e845a1a8f716cf71e99f19
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-06 16:43:35 +0600

    my-app added

--- frist-app/src/index.js
+++ frist-app/src/index.js
@@ -1,17 +1,7 @@
 import ReactDOM from "react-dom/client";
 
-const todoTitle = "This is Sadril";
-const todoDes = "I am sadril amin shuvo. Student of cse.";
-const date = new Date();
-const todoDate = date.getFullYear();
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render("root");
-root.render(
+ReactDOM.render(
   <div>
-    <h1>Todo app</h1>
-    <h3>{todoTitle}</h3>
-    <p>{todoDes}</p>
-    <p>{todoDate}</p>
+    <h1>Hello</h1>
   </div>
 );
