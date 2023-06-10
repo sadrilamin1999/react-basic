92f0f04e2efecbb26339c1d1f28daf89df87fe5b
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-10 13:45:05 +0600

    style added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title = "call family";
+const title = "Call family";
 const des = "This is frist class todoapp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>{title}</h1>
+      <h1 className="heading">{title}</h1>
       <p>{des}</p>
     </div>
   </React.StrictMode>
