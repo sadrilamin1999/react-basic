eef5939d15b0a3e3a6f6c8d5f0ec9428a1c8a151
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-12 12:11:23 +0600

    unique id added

--- card-app/src/App.js
+++ card-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "./components/Card";
 import Data from "./Data.json";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   return (
     <div>
       <h1 className="heading">Todo App</h1>
       {Data.map((item) => (
-        <Card title={item.title} des={item.des} />
+        <Card key={uuidv4()} title={item.title} des={item.des} />
       ))}
     </div>
   );
