38f0af512003b3f7c4485a71cb908204c0918415
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-06-22 12:20:07 +0600

    react icon added

---
+++ react-icon/src/App.js
@@ -0,0 +1,13 @@
+import { FcAbout, FcAddressBook } from "react-icons/fc";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>React Icons</h1>
+     <span><FcAbout/></span>
+     <span><FcAddressBook/></span>
+    </div>
+  );
+}
+
+export default App;
