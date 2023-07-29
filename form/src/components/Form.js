b482d34ea1cc517d04db8b0e99d53796c945e83b
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-07-29 17:54:28 +0600

    form added

---
+++ form/src/components/Form.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    console.log(name, email, password);
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h1>Registation</h1>
+
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="sumbit">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
