e73636f0314c8673eeb879ef3928227697021752
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-12 00:36:53 +0600

    react-router added

---
+++ react-router-intro/src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
