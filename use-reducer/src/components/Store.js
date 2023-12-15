8b14bb9204ea0d57ec9a41cc53e10aa481b026f7
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-12-15 16:45:37 +0600

    useReducer hook updated

--- use-reducer/src/components/Store.js
+++ use-reducer/src/components/Store.js
@@ -1,61 +1,46 @@
 import { useReducer } from "react";
 
-const initialState = {
-  pizza: 20,
-};
 const Store = () => {
+  const initialState = {
+    count: 20,
+  };
   const reducer = (state, action) => {
     switch (action.type) {
       case "buy":
-        return { ...state, pizza: state.pizza - action.payload };
+        return { ...state, count: state.count - action.payload };
 
       case "cancel":
-        return { ...state, pizza: state.pizza + action.payload };
+        return { ...state, count: state.count + action.payload };
 
-      case "add":
-        return { ...state, pizza: state.pizza + action.payload };
+      case "restock":
+        return { ...state, count: state.count + action.payload };
 
       case "reset":
-        return { ...state, pizza: (state.pizza = action.payload) };
+        return { ...state, count: (state.count = action.payload) };
 
       default:
         return { state };
     }
   };
-  const [shop, dispatch] = useReducer(reducer, initialState);
+  const [counter, dispatch] = useReducer(reducer, initialState);
   return (
-    <div>
-      <h2>Number of pizza = {shop.pizza}</h2>
-      <button
-        onClick={() =>
-          dispatch({
-            type: "buy",
-            payload: 1,
-          })
-        }
-      >
-        Buy
-      </button>
-
-      <button
-        onClick={() => {
-          dispatch({
-            type: "cancel",
-            payload: 1,
-          });
-        }}
-      >
+    <>
+      <h2>Total pizza = {counter.count}</h2>
+      <p>Ordered = 0 </p>
+      <button onClick={() => dispatch({ type: "buy", payload: 1 })}>Buy</button>
+      <button onClick={() => dispatch({ type: "cancel", payload: 1 })}>
         Cancel
       </button>
-
-      <button onClick={() => dispatch({ type: "add", payload: 10 })}>
-        Add
+      <button onClick={() => dispatch({ type: "restock", payload: 10 })}>
+        Restock
       </button>
 
-      <button onClick={() => dispatch({ type: "reset", payload: 0 })}>
+      <button
+        onClick={() => dispatch({ type: "reset", payload: initialState.count })}
+      >
         Reset
       </button>
-    </div>
+    </>
   );
 };
 
